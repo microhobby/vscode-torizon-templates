896a79b7bf6f78d7b9660aaeb077807786494053
alberrizwan <alber.rizwan@crossware.io>
2025-02-23 01:15:43 +0530

    Uses gdb for debugging

--- dartFlutter/run.sh
+++ dartFlutter/run.sh
@@ -5,7 +5,15 @@ if [ -z "$APP_PATH" ]; then
   echo "Error: APP_PATH environment variable is not set."
   exit 1
 fi
-export LD_LIBRARY_PATH=/opt/flutter-elinux/
-# Run the command using the environment variable
-/opt/flutter-embedded-linux/build/flutter-client --bundle="$APP_PATH" -f
+
+# Check RUNTIME variable
+if [ "$RUNTIME" == "debug" ]; then
+  echo "Running in Debug mode: Starting SSHD..."
+  /usr/sbin/sshd -D
+else
+# Set library path
+  export LD_LIBRARY_PATH=/opt/flutter-elinux/
+  echo "Running in Release mode: Starting Flutter client..."
+  /opt/flutter-embedded-linux/build/flutter-client --bundle="$APP_PATH" -f
+fi
 
